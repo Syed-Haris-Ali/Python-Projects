848629b8b75a3ce48088920a0cd337c78e252714
Syed Haris Ali <60552829+Syed-Haris-Ali@users.noreply.github.com>
2021-08-05 01:32:58 +0500

    bot.py

---
+++ Instagram Post Uploader/bot.py
@@ -0,0 +1,6 @@
+# By Codred Tech
+# pip install instabot
+from instabot import bot  
+bot = Bot()
+bot.login(username="user_name",  password="user_password")  #Instagram Login
+bot.upload_photo("YourPost.jpg", caption="Caption of Post Here") #Upload your post here
